ef70c104a2da4621541737685eb305cfc8748314
sawagroupdeveloper <mstfkhazaal@gmail.com>
2022-10-08 16:11:06 +0300

    Add Modules

---
+++ Modules/HR/Resources/js/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+    <app>
+       <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                HR
+            </h2>
+        </template>
+
+        <slot></slot>
+    </app>
+</template>
+
+<script>
+import App from '@/Layouts/Authenticated.vue'
+
+export default {
+    name: 'Layout',
+    components: {
+        App,
+    },
+}
+</script>
